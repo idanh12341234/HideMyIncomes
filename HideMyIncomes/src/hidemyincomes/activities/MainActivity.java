02e112921810e8096ae370a90b916f94fe489403
idan <idan@idan-PC>
2014-02-23 21:55:02 +0200

    footer

--- HideMyIncomes/src/hidemyincomes/activities/MainActivity.java
+++ HideMyIncomes/src/hidemyincomes/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package hidemyincomes.activities;
 
-import android.R;
+import com.example.hidemyincomes.R;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 	
@@ -10,5 +13,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_layout);
+		LinearLayout l = (LinearLayout) findViewById(R.id.footer);
+		l.setBackgroundColor(Color.BLACK);
 	}
 }
